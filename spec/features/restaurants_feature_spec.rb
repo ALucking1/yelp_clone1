de28e1686137af074b88eb8fce1f06b08a6e4307
Anna Lucking <annavlucking@hotmail.co.uk>
2016-11-15 14:08:51 +0000

    for_form

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -19,4 +19,15 @@ feature 'restaurants' do
       expect(page).not_to have_content('No restaurants yet')
     end
   end
+
+    context 'creating restaurants' do
+      scenario 'prompts user to fill out a form, then displays the new restaurant' do
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'Waffle House'
+        click_button 'Create Restaurant'
+        expect(page).to have_content 'Waffle House'
+        expect(current_path).to eq '/restaurants'
+      end
+    end
 end
