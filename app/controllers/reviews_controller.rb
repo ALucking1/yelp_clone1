f543103f660bb6cb94ed8f883ce144015d7ebcad
Anna Lucking <annavlucking@hotmail.co.uk>
2016-11-18 17:22:41 +0000

    users can only leave one review - delete/edit not working yet

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,26 +4,17 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
-def build_review(attributes = {}, user)
-  review = reviews.build(attributes)
-  review.user = user
-  review
-end
+  def build_review(attributes = {}, user)
+    review = reviews.build(attributes)
+    review.user = user
+    review
+  end
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.build_review review_params, current_user
-
-if @review.save
-  redirect_to restaurants_path
-else
-  if @review.errors[:user]
-    redirect_to restaurants_path, alert: 'You have already reviewed this restaurant'
-  else
-    render :new
+    @restaurant.reviews.create(review_params)
+    redirect_to '/restaurants'
   end
- end
-end
 
   private
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,12 +1,10 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :user
-  has_many :reviews, dependent: :destroy
+  # has_many :reviews, dependent: :destroy
   validates :name, length: { minimum: 3}, uniqueness: true
-  has_many :reviews do
-  def build_with_user(attributes = {}, user)
-    attributes[:user] ||= user
-    build(attributes)
-  end
-end
+
+  has_many :reviews,
+      -> { extending WithUserAssociationExtension },
+      dependent: :destroy
 
 end
